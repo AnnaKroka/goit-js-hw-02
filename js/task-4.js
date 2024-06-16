be29009eed45edcae05212fda2cdcab9940f2c4a
Anna Kroka <avkroka.broker@gmail.com>
2024-06-16 13:08:25 +0300

    add task4 and modify other tasks

---
+++ js/task-4.js
@@ -0,0 +1,10 @@
+function getShippingCost(country) {
+    let price = 2;
+    let message = '';
+    switch (price) {
+        case 1: {
+            message = `Shipping to ${country} will cost ${price} credits`;
+            break;
+        }
+    }
+}
